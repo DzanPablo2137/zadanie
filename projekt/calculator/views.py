9b69655702cc4f9c2297fe0c336ad910ab8eec4e
Franek <franekfbb@gmail.com>
2023-11-20 10:04:22 -0500

    2

---
+++ projekt/calculator/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def hello(request, number):
+    print(request)
+    return HttpResponse(f"Hello, Django!  {number}")
