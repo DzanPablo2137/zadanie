ce2ab33334d63272989c261b8fd7ca269648b3c6
Franek <franekfbb@gmail.com>
2023-11-27 09:28:23 -0500

    11/27

--- projekt/calculator/urls.py
+++ projekt/calculator/urls.py
@@ -1,7 +1,8 @@
-from  .views import hello, calc
+from  .views import get_users, hello, calc
 from django.urls import path
 
 urlpatterns = [
     path('hello/<int:number>', hello),
     path('calc', calc),
+    path('users', get_users),
 ]
--- projekt/calculator/views.py
+++ projekt/calculator/views.py
@@ -4,6 +4,8 @@ import json
 # Create your views here.
 from django.http import HttpResponse
 
+from calculator.models import User
+
 def hello(request, number):
     print(request)
     return HttpResponse(f"Hello, Django!  {number}")
@@ -11,4 +13,9 @@ def hello(request, number):
 @csrf_exempt
 def calc(request):
     data = json.loads(request.body)
-    return HttpResponse(f"")
+    if data["operation"] == "+":
+        result = data["a"] + data["b"]
+    return HttpResponse(f"{result}")
+
+def get_users(request):
+    users = User.object.all()
